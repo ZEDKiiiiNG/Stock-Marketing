35c590867727fdb0660e5db23849af9e91e5ca6f
tong shen <ts325@duke.edu>
2022-03-18 23:57:57 -0400

    recv response

--- Client.cpp
+++ Client.cpp
@@ -9,14 +9,18 @@ int main(int argc, char *argv[]) {
     Socket socket;
     int msg_fd = socket.setupClient(HOST, PORT);
     std::string request = "173\n"
-                          "  <?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-                          "  <create>\n"
+                          "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                          "<create>\n"
                           "    <account id=\"123456\" balance=\"1000\"/>\n"
                           "    <symbol sym=\"SPY\">\n"
-                          "      <account id=\"123456\">100000</account>\n"
+                          "        <account id=\"123456\">100000</account>\n"
                           "    </symbol>\n"
                           "</create>";
     socket.sendMesg(msg_fd, request);
+
+    std::vector<char> response = socket.recvMesg(msg_fd);
+    std::cout << response.data() << '\n';
+
     socket.closeConn(msg_fd);
     return EXIT_SUCCESS;
 }
