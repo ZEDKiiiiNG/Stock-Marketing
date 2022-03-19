af2eaa5b4a968c0dd1f6be016bd1969993b7ac73
tong shen <ts325@duke.edu>
2022-03-18 23:23:22 -0400

    recv request from client

--- Server.cpp
+++ Server.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "Server.h"
+
+int main(int argc, char *argv[]) {
+    Socket socket;
+    int listen_fd = socket.setupServer(PORT);
+    int msg_fd = socket.acceptConn(listen_fd);
+    std::vector<char> request = socket.recvMesg(msg_fd);
+    std::cout << request.data() << '\n';
+    return EXIT_SUCCESS;
+}
--- Server.h
+++ Server.h
@@ -2,9 +2,13 @@
 // Created by Tong on 3/18/22.
 //
 
-#ifndef _SERVER_H
+#ifndef _SERVER_H_
 #define _SERVER_H_
 
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include "Socket.h"
 
 class Server {
 
