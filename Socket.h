b72e3d64123f360b17a490d0fc45fe67882d10c2
tong shen <ts325@duke.edu>
2022-03-24 17:33:04 -0400

    use constant

--- Socket.h
+++ Socket.h
@@ -14,12 +14,7 @@
 #include <arpa/inet.h>
 #include <string>
 #include <cstring>
-
-#define BAGLOG 10
-#define LARGE_BUFFER_LEN 1000000
-#define SMALL_BUFFER_LEN 8000
-#define HOST "vcm-24846.vm.duke.edu"
-#define PORT "12345"
+#include "Constant.h"
 
 class Socket {
 private:
