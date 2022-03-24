81f859138988363af9863329e2a970a9420eed97
tong shen <ts325@duke.edu>
2022-03-24 17:28:08 -0400

    use constant

---
+++ Constant.h
@@ -0,0 +1,14 @@
+//
+// Created by Tong on 3/24/22.
+//
+
+#ifndef _CONSTANT_H_
+#define _CONSTANT_H_
+
+#define BAGLOG 10
+#define LARGE_BUFFER_LEN 1000000
+#define SMALL_BUFFER_LEN 8000
+#define HOST "vcm-24846.vm.duke.edu"
+#define PORT "12345"
+
+#endif
