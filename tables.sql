38f328c1e1220ba7a599ba17668866fdb839cb0f
tong shen <ts325@duke.edu>
2022-03-24 19:33:29 -0400

    change table schema

--- tables.sql
+++ tables.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS account CASCADE;
 
 CREATE TABLE account (
-    account_id,
-    balance,
+    account_id INT NOT NULL,
+    balance INT DEFAULT 0,
     PRIMARY KEY (account_id)
 );
