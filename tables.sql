a0f152bf9bf2fb1b467bae75108446dee9b06a16
tong shen <ts325@duke.edu>
2022-03-26 13:01:08 -0400

    reference account

--- tables.sql
+++ tables.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS account CASCADE;
 
 CREATE TABLE account (
-    account_id INT NOT NULL,
+    account_id INT,
     balance DOUBLE PRECISION DEFAULT 0,
     PRIMARY KEY (account_id)
 );
@@ -9,5 +9,6 @@ CREATE TABLE account (
 CREATE TABLE position (
     symbol VARCHAR(256),
     amount DOUBLE PRECISION DEFAULT 0,
+    account_id INT,
     FOREIGN KEY (account_id) REFERENCES account(account_id) ON DELETE SET NULL ON UPDATE CASCADE
 );
