e71fb423bafef9ec42e12384d920e8b4e60251a4
tong shen <ts325@duke.edu>
2022-03-26 17:53:09 -0400

    add default val

--- tables.sql
+++ tables.sql
@@ -21,8 +21,8 @@ CREATE TABLE trade_order (
     amount DOUBLE PRECISION,
     limit_price DOUBLE PRECISION,
     status VARCHAR(256) DEFAULT 'open',
-    update_time INT,
-    execute_price DOUBLE PRECISION,
+    update_time INT default 0,
+    execute_price DOUBLE PRECISION default 0,
     account_id INT,
     PRIMARY KEY (order_id),
     FOREIGN KEY (account_id) REFERENCES account(account_id) ON DELETE SET NULL ON UPDATE CASCADE
