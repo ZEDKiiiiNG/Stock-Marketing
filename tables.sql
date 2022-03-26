6a4f1ffa6d54eea90c28ad106ea6b87d7192bf17
tong shen <ts325@duke.edu>
2022-03-26 12:52:28 -0400

    add test

--- tables.sql
+++ tables.sql
@@ -2,6 +2,12 @@ DROP TABLE IF EXISTS account CASCADE;
 
 CREATE TABLE account (
     account_id INT NOT NULL,
-    balance INT DEFAULT 0,
+    balance DOUBLE DEFAULT 0,
     PRIMARY KEY (account_id)
+);
+
+CREATE TABLE position (
+    symbol VARCHAR(256),
+    amount DOUBLE DEFAULT 0,
+    FOREIGN KEY (account_id) REFERENCES STATE(account_id) ON DELETE SET NULL ON UPDATE CASCADE,
 );
