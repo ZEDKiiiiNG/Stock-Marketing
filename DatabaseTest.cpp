5558d7c33e6dbec03bf1eda205e8f10d10d9f654
tong shen <ts325@duke.edu>
2022-03-26 20:16:07 -0400

    rm print

--- DatabaseTest.cpp
+++ DatabaseTest.cpp
@@ -7,13 +7,6 @@
 void DatabaseTest::testSaveAccount() {
     db.saveAccount(1, 10000);
     db.saveAccount(2, 1500);
-    pqxx::nontransaction n(*db.conn);
-    std::string query("SELECT * FROM account;");
-    pqxx::result r(n.exec(query));
-    for (pqxx::result::const_iterator c = r.begin(); c != r.end(); ++c) {
-        std::cout << c[0].as<int>() << " "
-                  << c[1].as<int>() << "\n";
-    }
 }
 
 void DatabaseTest::testHasAccount() {
