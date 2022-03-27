2ba4a6a134459baa8ae17d3ce9348988981c196c
ZEDKIIIING <296213663@qq.com>
2022-03-26 21:47:13 -0400

    xl351-08-order create initial

--- Server.h
+++ Server.h
@@ -17,13 +17,20 @@ class Server {
 //std::string getXmlContent(const std::string& raw);
 private:
     Database db;
-
+    int orderId;
     void handleCreate(TiXmlElement* rootElement, TiXmlElement* rootResultElement);
     void handleTransection(TiXmlElement* rootElement, TiXmlElement* rootResultElement);
 
     void handleAccountCreate(TiXmlElement* rootElement, TiXmlElement* rootResultElement);
     void handleSymbolCreate(TiXmlElement* rootElement, TiXmlElement* rootResultElement);
+
+    void handleOrderTransection(TiXmlElement* rootElement, TiXmlElement* rootResultElement, int accountId);
+    void handleQueryTransection(TiXmlElement* rootElement, TiXmlElement* rootResultElement, int accountId);
+    void handleCancelTransection(TiXmlElement* rootElement, TiXmlElement* rootResultElement, int accountId);
 public:
+    Server(){
+        orderId = 0;
+    }
     const char * getXmlContent(const char* raw);
 
     void handleRequest(TiXmlElement* rootElement, TiXmlElement* rootResultElement);
