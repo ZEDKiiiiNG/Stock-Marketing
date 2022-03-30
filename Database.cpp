1955fc045b586fff16ddf40ca9ff8ca029b523dd
tong shen <ts325@duke.edu>
2022-03-30 16:30:32 -0400

    test cancel

--- Database.cpp
+++ Database.cpp
@@ -183,7 +183,7 @@ void Database::updateCancelOrder(int orderId, int accountId) {
        << " SET status = " << w.quote(STATUS_CANCELLED)
        << ", update_time = " << time(NULL)
        << " WHERE account_id = " << accountId << " AND order_id = " << orderId
-       << " AND status = " << STATUS_OPEN << ";";
+       << " AND status = " << w.quote(STATUS_OPEN) << ";";
     w.exec(ss.str());
     w.commit();
 }
