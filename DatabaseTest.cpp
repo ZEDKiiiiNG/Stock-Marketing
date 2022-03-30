078a19c5d0d85380444abb572162633fb38e9d52
tong shen <ts325@duke.edu>
2022-03-30 16:39:08 -0400

    test

--- DatabaseTest.cpp
+++ DatabaseTest.cpp
@@ -149,6 +149,8 @@ void DatabaseTest::testHandleSell() {
     db.saveOrder(6, "TEA", -8, 110, STATUS_OPEN, 0, 4);
     pqxx::result r = db.getBuyOrder(110, "TEA");
     displayOrder(r);
+    r = getBuyOrder(110, "TEA");
+    displayOrder(r);
 
     db.updateOpenOrder(3, 3, 4);
     r = db.getOrder(3, 3);
