32607fc1a4306a7e68bf19acca1f46014f8eb167
tong shen <ts325@duke.edu>
2022-04-01 15:33:31 -0400

    refactor

--- Database.cpp
+++ Database.cpp
@@ -180,13 +180,11 @@ void Database::placeOrder(pqxx::connection * conn, int orderId, std::string symb
         std::cout << e.what() << '\n';
         w.abort();
     }
-    /*
     if (amount < 0) {
         handleSellOrder(conn, orderId, symbol, accountId, amount, limitPrice);
     } else {
         handleBuyOrder(orderId, symbol, accountId, amount, limitPrice);
     }
-     */
 
 }
 
