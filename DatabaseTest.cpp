0c0d86aa50f188a80c40b49efb96684ab90b191e
tong shen <ts325@duke.edu>
2022-03-31 21:33:10 -0400

    test multi

--- DatabaseTest.cpp
+++ DatabaseTest.cpp
@@ -257,7 +257,7 @@ void DatabaseTest::testUpdateAmountMulti() {
     double amount2 = 6;
     db.saveAccount(accountId, 10000);
     db.savePosition(symbol, accountId);
-    // db.updateAmount(symbol, accountId, 5);
+    db.updateAmount(symbol, accountId, 5);
     std::thread (&Database::updateAmount, this->db, symbol, accountId, amount1).detach();
     std::thread (&Database::updateAmount, this->db, symbol, accountId, amount2).detach();
     // std::thread t2(&Database::updateAmount, this->db, symbol, accountId, amount2);
