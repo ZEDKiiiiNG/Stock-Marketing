59212dd3e1ad04a995402332ebf188a74880d06b
tong shen <ts325@duke.edu>
2022-04-01 14:59:03 -0400

    test

--- Database.cpp
+++ Database.cpp
@@ -280,7 +280,7 @@ std::string Database::getExecuteBuyOrderQuery(pqxx::work *w, int buyOrderId, std
     std::stringstream ss;
     ss << getUpdatePositionQuery(w, symbol, buyerAccountId, executeAmount)
         << "\n" << getUpdateBalanceQuery(w, buyerAccountId, executeAmount * (buyLimit - executePrice))
-        << "\n" << getUpdateOpenOrdeQuery(w, buyOrderId, buyerAccountId, remainAmount)
+        << "\n" << getUpdateOpenOrderQuery(w, buyOrderId, buyerAccountId, remainAmount)
         << "\n" << getSaveOrderQuery(w, buyOrderId, symbol, executeAmount, 0, STATUS_EXECUTED, executePrice, buyerAccountId);
     return ss.str();
 }
