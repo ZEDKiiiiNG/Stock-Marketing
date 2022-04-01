2aed58a808f3d92bd7314c2b51febd7d4aa663a8
tong shen <ts325@duke.edu>
2022-04-01 17:14:16 -0400

    test

--- DatabaseTest.cpp
+++ DatabaseTest.cpp
@@ -55,24 +55,25 @@ void DatabaseTest::testOrder() {
     assert(db.getBalance(1) == 4860); // deduct balance
     assert(db.getAmount("SYM", 1) == 228.8);
 }
+ */
 
 void DatabaseTest::testException() {
     try {
-        db.updatePosition("SYM", 5, 100);
+        db.updatePosition(testconn, "SYM", 5, 100);
     } catch (std::invalid_argument & e) {
         std::cout << e.what() << '\n';
         assert(std::string(e.what()) == ACCOUNT_NOT_EXIST_ERROR);
     }
 
     try {
-        db.createAccount(1, 200);
+        db.createAccount(testconn, 1, 200);
     } catch (std::invalid_argument & e) {
         std::cout << e.what() << '\n';
         assert(std::string(e.what()) == ACCOUNT_EXIST_ERROR);
     }
 
     try {
-        db.placeOrder(1, "SYM", 1, 3, 1650); // buy
+        db.placeOrder(testconn, 1, "SYM", 1, 3, 1650); // buy
     } catch (std::invalid_argument & e) {
         std::cout << e.what() << '\n';
         assert(std::string(e.what()) == INSUFFICIENT_BALANCE_ERROR);
@@ -80,7 +81,7 @@ void DatabaseTest::testException() {
     }
 
     try {
-        db.placeOrder(1, "BTC", 2, -11, 110); // sell
+        db.placeOrder(testconn, 1, "BTC", 2, -11, 110); // sell
     } catch (std::invalid_argument & e) {
         std::cout << e.what() << '\n';
         assert(std::string(e.what()) == INSUFFICIENT_SHARE_ERROR);
@@ -88,13 +89,13 @@ void DatabaseTest::testException() {
     }
 
     try {
-        db.placeOrder(1, "BTC", 3, -5, 110);
+        db.placeOrder(testconn, 1, "BTC", 3, -5, 110);
     } catch (std::invalid_argument & e) {
         std::cout << e.what() << '\n';
         assert(std::string(e.what()) == ACCOUNT_NOT_EXIST_ERROR);
     }
 }
-*/
+
 
 void DatabaseTest::testCancel() {
     /*
@@ -435,10 +436,10 @@ int main(int argc, char *argv[]) {
     test.testSaveAccount();
     test.testHasAccount();
     test.testPosition();
-    /*
-    test.testOrder();
+
+    // test.testOrder();
     test.testException();
-     */
+
     test.testCancel();
 
     test.testHandleSell();
