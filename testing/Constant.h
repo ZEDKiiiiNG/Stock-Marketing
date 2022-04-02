55f4734551956d60a8a3ff9778c43519d5bfb1ee
tong shen <ts325@duke.edu>
2022-04-01 21:49:15 -0400

    move files

---
+++ testing/Constant.h
@@ -0,0 +1,23 @@
+//
+// Created by Tong on 3/24/22.
+//
+
+#ifndef _CONSTANT_H_
+#define _CONSTANT_H_
+
+#define BAGLOG 10
+#define LARGE_BUFFER_LEN 8000
+#define SMALL_BUFFER_LEN 8000
+#define HOST "vcm-24622.vm.duke.edu"
+#define PORT "12345"
+#define DB_INFO "dbname=testdb user=postgres password=postgres"
+#define ACCOUNT_EXIST_ERROR "Account already exists"
+#define ACCOUNT_NOT_EXIST_ERROR "Account does not exist"
+#define INSUFFICIENT_BALANCE_ERROR "Insufficient balance are available"
+#define INSUFFICIENT_SHARE_ERROR "Insufficient shares are available"
+#define NO_OPEN_ORDER_ERROR "Does not have an open order"
+#define STATUS_OPEN "open"
+#define STATUS_CANCELLED "cancelled"
+#define STATUS_EXECUTED "executed"
+
+#endif
